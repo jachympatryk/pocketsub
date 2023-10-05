677df74e021977fed546285d1bf670d97cf16d47
patrykjachym <jachympatryk99@gmail.com>
2023-10-05 10:19:43 +0200

    nav & auth

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,4 +1,6 @@
+import { AuthButtons } from "@/components";
 import type { Metadata } from "next";
+import Link from "next/link";
 import React from "react";
 
 export const metadata: Metadata = {
@@ -8,8 +10,20 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex">
-      <header>navigaition</header>
+    <div>
+      <header className="mx-auto flex max-w-7xl items-center p-6 gap-2">
+        <Link href={"/"}>
+          <h1 className="font-bold text-xl">Pocketsub</h1>
+        </Link>
+        <nav className="w-full">
+          <ul className="flex items-center justify-center gap-4 list-none">
+            <li>
+              <Link href={"/blog"}>Blog</Link>
+            </li>
+          </ul>
+        </nav>
+        <AuthButtons />
+      </header>
       {children}
     </div>
   );
