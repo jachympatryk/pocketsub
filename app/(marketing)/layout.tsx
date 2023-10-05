f9f8cc66feb7ca8c6dd33e4a3d350e2c6a8068c1
patrykjachym <jachympatryk99@gmail.com>
2023-10-05 11:23:58 +0200

    hero page styles

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,19 +1,16 @@
 import { AuthButtons } from "@/components";
-import type { Metadata } from "next";
+import logo from "../../public/img/Logo.png";
 import Link from "next/link";
 import React from "react";
-
-export const metadata: Metadata = {
-  title: "Pocketsub",
-  description: "Manage your payments",
-};
+import Image from "next/image";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <div>
       <header className="mx-auto flex max-w-7xl items-center p-6 gap-2">
-        <Link href={"/"}>
-          <h1 className="font-bold text-xl">Pocketsub</h1>
+        <Link href={"/"} className="flex items-center gap-2">
+          <Image src={logo} alt="Pocketsub logo" height={34} />
+          <h1 className="text-xl font-bold">Pocketsub</h1>
         </Link>
         <nav className="w-full">
           <ul className="flex items-center justify-center gap-4 list-none">
